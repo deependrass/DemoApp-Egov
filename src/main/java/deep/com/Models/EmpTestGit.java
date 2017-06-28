5c8f3a27ecb8f13c90df0734aa8dd298a40ee8d9
Deependra Singh Shekhawat <deependra.shekhawat@goodwrklabs.com>
2017-06-28 17:41:33 +0530

    New varible added

--- src/main/java/deep/com/Models/EmpTestGit.java
+++ src/main/java/deep/com/Models/EmpTestGit.java
@@ -3,6 +3,15 @@ package deep.com.Models;
 public class EmpTestGit {
 	
 	private String name;
+	private String Address;
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
+	}
 
 	public String getName() {
 		return name;

27ed6c3a1c66df74d754bdbc4fb563d40e58d8a8
Deependra Singh Shekhawat <deependra.shekhawat@goodwrklabs.com>
2017-06-28 17:20:16 +0530

    Git new file test

---
+++ src/main/java/deep/com/Models/EmpTestGit.java
@@ -0,0 +1,15 @@
+package deep.com.Models;
+
+public class EmpTestGit {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
